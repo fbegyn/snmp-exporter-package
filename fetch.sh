041bf2cc3c27295a00bae7d76c79dde2823026ff
Francis Begyn <francis@begyn.be>
2022-09-17 22:58:47 +0200

    update to new nfpm layout

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,5 @@
-#!/bin/env bash
+#! /usr/bin/env bash
 curl -fSL -o "./snmp_exporter-$1.linux-$2.tar.gz" \
   "https://github.com/prometheus/snmp_exporter/releases/download/v$1/snmp_exporter-$1.linux-$2.tar.gz"
-
 tar xzvf ./snmp_exporter-$1.linux-$2.tar.gz
-
-# get binaries
-mkdir ./bin
-cp ./snmp_exporter-$1.linux-$2/snmp_exporter ./bin/
-cp ./snmp_exporter-$1.linux-$2/snmp.yml ./
+cp ./snmp_exporter-$1.linux-$2/* ./
--- package.sh
+++ package.sh
@@ -1,9 +1,11 @@
-#!/bin/env bash
+#! /usr/bin/env bash
 
 ./fetch.sh $1 $2
 
 sed "s/vx.y.z/v$1/g" ./nfpm.yaml > ./nfpm-temp.yaml
 
-nfpm pkg --config="nfpm-temp.yaml" --target="snmp-exporter_$1_$2.deb"
+nfpm pkg --packager deb --config="nfpm-temp.yaml"
+nfpm pkg --packager rpm --config="nfpm-temp.yaml"
 
-rm -rf ./bin ./snmp.yml ./snmp_exporter-$1.linux-$2 ./snmp_exporter-$1.linux-$2.tar.gz ./nfpm-temp.yaml
+rm -rf ./snmp_exporter ./snmp.yml ./snmp_exporter-$1.linux-$2 \
+	./snmp_exporter-$1.linux-$2.tar.gz ./nfpm-temp.yaml ./LICENSE ./NOTICE
